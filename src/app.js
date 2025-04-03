1fbe4837c38715e56708ce2453cfb357f11ff24c
Raj-Ayush <rajayush8571@gmail.com>
2025-04-04 00:07:19 +0530

    fix(aremove credentials true issue)

--- src/app.js
+++ src/app.js
@@ -16,6 +16,7 @@ const fileUpload = require("express-fileupload");
 // const usersRouter = require('./routes/users');
 // const authRouter = require('./routes/auth.route');
 const { errorConverter, errorHandler } = require('./features/error');
+const configuration = require('../config');
 
 const app = express();
 
@@ -27,7 +28,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({
+    origin: configuration
+}));
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.get('/api/v1/health', (req, res) => {
