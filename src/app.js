4b7c8b35bd0b1fb0b5c4f9c782dadcf9d527b710
Raj-Ayush <rajayush8571@gmail.com>
2025-06-02 01:41:27 +0530

    fix(added cors issue)

--- src/app.js
+++ src/app.js
@@ -65,7 +65,7 @@ app.use(cors({
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
-app.options('*', cors());
+// app.options('*', cors());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
